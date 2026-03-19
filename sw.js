a3c7ad9ed5d11532eafa5779f6e021bf5da05043
THESHMIL0 <loolk448@gmail.com>
2026-03-20 00:34:54 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,10 +1,7 @@
-// This is a simple Service Worker to allow PWA Installation
-
 self.addEventListener('install', (e) => {
   console.log('Service Worker: Installed');
 });
 
-// A fetch listener is required by browsers to pass the PWA check
 self.addEventListener('fetch', (e) => {
-  // We aren't caching anything yet, just returning the normal network request
+  // Required for PWA install prompt to fire
 });
